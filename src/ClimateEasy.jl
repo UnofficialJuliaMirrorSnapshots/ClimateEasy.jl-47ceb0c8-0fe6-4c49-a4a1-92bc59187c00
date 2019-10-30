e851f2f2422894782ba1b7ef076d35e2c3524686
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 10:37:40 +0000

    Snapshot of branch master taken on 2019-10-30T06:37:39.711-04:00 from "https://github.com/UnofficialJuliaMirror/ClimateEasy.jl-47ceb0c8-0fe6-4c49-a4a1-92bc59187c00"

---
+++ src/ClimateEasy.jl
@@ -0,0 +1,44 @@
+module ClimateEasy
+
+# Main file for the ClimateEasy module that provides a range of useful
+# commands written in Julia that allows one to easily perform commonly-needed
+# tasks such as:
+#   - Converting dates and times to string format (e.g. YYYYMMDD)
+#   - Generating vectors of dates
+#   - Decomposing datestrings into their relevant components
+#   - Plotting on maps (requires CartoPy in conda package manager)
+
+## Modules Used
+using Dates, DelimitedFiles, Printf
+using PyCall, PyPlot
+
+## Adding relevant Python Functions
+const ccrs  = PyNULL()
+const pyhdf = PyNULL()
+
+function __init__()
+    copy!(ccrs,pyimport_conda("cartopy","cartopy"))
+    copy!(pyhdf,pyimport_conda("pyhdf","pyhdf","conda-forge"))
+end
+
+## Exporting functions
+export
+       yr2str, mo2str, dy2str, hr2str, mi2str, yrmo2str, ymd2str, ymdhm2str,
+       yrmo2dir, ymd2dir, yrdy2dir,
+       regionload, regioninfodisplay, regionshortname, regionfullname, regionparent,
+       regionbounds, regionpoint, regiongrid,
+       regionextract, regionextractpoint, regionextractgrid,
+       from0360to180, from180to0360,
+       convert2hourly, convert23hourly, convert2daily,
+       convert2regionmean, convert2zonalmean, convert2meridionalmean,
+       mollweide, robinson, coastlines,
+       h4read
+
+include("dnt2str.jl")
+include("regions.jl")
+include("processing.jl")
+include("hdf4.jl")
+include("mapping.jl")
+include("misc.jl")
+
+end # module
